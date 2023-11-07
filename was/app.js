dda67b514b9aab26e245d887dd8a0fb1c344a5ea
hee-bin <gmlqls4732@naver.com>
2023-11-07 22:44:05 +0900

    was mysql connect check

--- was/app.js
+++ was/app.js
@@ -7,24 +7,42 @@ const port = 3000;
 app.use(cors());
 
 const db = mysql.createConnection({
-  host: '10.98.165.174', // MySQL 서비스의 내부 IP 또는 DNS 이름
+  host: '10.98.165.174',
   user: 'root',
   password: process.env.MYSQL_ROOT_PASSWORD,
   database: 'testDB'
 });
 
+// 데이터베이스 연결
 db.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
   console.log("MySQL Connected!");
+
+  // 서버 시작 시 데이터베이스에서 메시지를 가져와서 확인합니다.
+  db.query('SELECT * FROM messages', (err, results) => {
+    if (err) {
+      console.error('Error querying the database:', err);
+    } else {
+      console.log('Initial messages from the database:', results);
+    }
+  });
 });
 
+// 클라이언트 요청에 대한 응답으로 데이터베이스에서 메시지를 가져옵니다.
 app.get('/', (req, res) => {
   db.query('SELECT * FROM messages', (err, results) => {
-    if (err) throw err;
+    if (err) {
+      console.error('Error querying the database:', err);
+      res.status(500).send('Error querying the database');
+      return;
+    }
     res.send(results);
   });
 });
 
 app.listen(port, () => {
-  console.log(`WAS listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
