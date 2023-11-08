ce097d1a6a46e0b649b47e4984d072edd2439c1e
hee-bin <gmlqls4732@naver.com>
2023-11-08 12:31:58 +0900

    App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 
 function App() {
   const [data, setData] = useState(null);
@@ -23,18 +22,30 @@ function App() {
       });
   }, []);
 
+  const appStyle = {
+    backgroundColor: 'black', // 검은 바탕
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // 화면 전체 높이에 가운데 정렬
+    color: 'white', // 흰색 글자
+    fontSize: '24px', // 원하는 글꼴 크기
+  };
+
   return (
-    <div className="App">
-      <h1 className="App-header">Messages 4.9.4</h1>
-      {error ? ( // 에러 상태에 따라 에러 메시지 또는 데이터를 표시
-        <div>Error: {error}</div>
-      ) : (
-        <ul className="App-list">
-          {data ? data.map((message, index) => (
-            <li key={index}>{message.content}</li>
-          )) : <li>Loading messages...</li>}
-        </ul>
-      )}
+    <div style={appStyle}>
+      <div>
+        <h1>Messages 4.9.5</h1>
+        {error ? ( // 에러 상태에 따라 에러 메시지 또는 데이터를 표시
+          <div>Error: {error}</div>
+        ) : (
+          <ul>
+            {data ? data.map((message, index) => (
+              <li key={index}>{message.content}</li>
+            )) : <li>Loading messages...</li>}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
